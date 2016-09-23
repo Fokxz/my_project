6ea7a6c1f3704da76bde1566b50e3b1dec4623db
Fokxz <walik1995@yandex.ru>
2016-09-23 10:28:17 +0300

    Init

--- gulpfile.js
+++ gulpfile.js
@@ -42,8 +42,8 @@ gulp.task('styles', function () {
     //.pipe(browserSync.stream());
 });
 
-gulp.task('watch', function () {
-    gulp.watch(['src/sass/*.sass', 'app/sass/**/*.sass',], ['styles']);
-});
+gulp.task('watch', function () { 
+gulp.watch(['src/sass/*.sass', 'src/sass/**/*.sass', 'src/sass/**/*.scss' ], ['styles']); 
+}); 
 
-gulp.task('default', [ 'browser-sync', 'watch']);
+gulp.task('default', ['watch']);
